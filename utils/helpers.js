3293c2cff915b6ab3de1f8a08b2c124eff512232
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2024-01-02 17:41:07 -0600

    FIX DATE FORMAT

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,7 @@
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    // const newDate = new Date(date);
-
-    // const day = newDate.getDate().toString().padStart(2, '0');
-    // const month = (newDate.getMonth() + 1).toString().padStart(2, '0');
-    // const year = newDate.getFullYear();
-
-    return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${new Date(date).getFullYear()}`;
+    
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`
   }
 };
+
